8390c79e5a5d4c65f069bf523901b4d3c53d3c25
James Ravenscroft <44760378+jamesravenscroft@users.noreply.github.com>
2019-02-23 22:05:24 -0800

    added letters js

---
+++ letter.js
@@ -0,0 +1,8 @@
+//constructor Letter
+//display underlying or placeholder
+//depending on whether use has guessed letter
+//1,string value to the store underlying character for letter
+//2, boolean value that stores wheter that has been guessed.
+//3function that returns the underlgin character if letter has been guessed. 
+//4 functio that takes characters as arguements, checks against underlying
+//character, updates stored boolean
